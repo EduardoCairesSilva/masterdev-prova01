3982c5479521e0d4d374e85c015476a9c12581e4
Eduardo Caires Silva <yandudu743@gmail.com>
2024-03-02 14:58:35 -0300

    Nova atualização

--- src/marminino.js
+++ src/marminino.js
@@ -1,8 +1,12 @@
 function executar(numero) {
-    return "";
-    //return "Mar";
-    //return "Minino"
-    //return "MarMinino";
+    if (numero % 3 === 0 && numero % 4 === 0) {
+        return "MarMinino;"
+    } else if (numero % 3 === 0) {
+        return "Mar";
+    } else if (numero % 4 === 0) {
+        return "Minino";
+    } else {
+        return "";
+    }
 }
-
 module.exports = executar;
